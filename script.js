c48a66f7135ff2889c1f998e00963469711a2dd3
Sanu <sodiumgamming111@gmail.com>
2026-08-13 15:34:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,17 +16,17 @@ const audioTracks = [
     {
         title: "I think they call this love ❤️",
         artist: "Listin when you feel happy 🙂",
-        src: "22.mpeg"
+        src: "audio/22.mpeg"
     },
     {
         title: "I Thought i saw your face today 💕",
         artist: "Listin when u feel fresh 😇",
-        src: "23.mpeg"
+        src: "audio/23.mpeg"
     },
     {
         title: "Chaar Kadam 😚",
         artist: " Listin When u thought nobody cares about u 😓",
-        src: "24.mpeg"
+        src: "audio/24.mpeg"
     }
 ];
 // Gallery images for puzzle
